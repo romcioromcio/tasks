fc1d1a253f49a9197430436a1935524aa13d5595
“nasz_login_do_github <nasz_email_do_github>
2018-01-18 22:38:06 +0100

    First attempt to Heroku

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-//@SpringBootApplication
+@SpringBootApplication
 //public class TasksApplication extends SpringBootServletInitializer {
 
 public class TasksApplication {
