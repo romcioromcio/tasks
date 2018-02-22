76f2fa1c4dea20c8dc13c831e6bb20cec1b76eb5
“romcioromcio <sobczynskir@wp.pl>
2018-02-22 23:07:19 +0100

    19.3-Mail_toCc

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+    private String mailTo;
+    private String subject;
+    private String toCc;
+    private String message;
+}
